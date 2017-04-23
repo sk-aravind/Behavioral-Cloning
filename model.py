9c033534e38a86576bd88634cc6f6cae2b14fbe5
Skara <sk.aravind.93@gmail.com>
2017-04-23 13:07:56 +0800

    Added version with generator

--- model.py
+++ model.py
@@ -1,89 +1,141 @@
 import csv
 import cv2
 import numpy as np
-import helper
+from helper import get_file_name
+from helper import augment_data
+from helper import mirror
 from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D, Dropout, Cropping2D, Convolution2D
 
-###================== Loading Data ==================###
+
+###=====================================================###
+###================== Hyperparameters ==================###
+###=====================================================###
+
+
+input_shape = (160, 320, 3)
+validation_split = 0.2
+steering_correction = 0.17
+dropout = 0.2
+optimizer = 'adam'
+loss_function = 'mse'
+epochs = 5
+batch_size = 128
+
+'''
+Parameters for controlling the amount of a certain type of data that is generated
+Such as the number of left camera images or the amount of augmented images in each batch
+Parameters for type of augmentations can be found in helper.py
+'''
+
+left_cam_limit = 0.3
+right_cam_limit = 0.3
+aug_limit = 0.3
+
+
+###=====================================================###
+###================== Loading Data =====================###
+###=====================================================###
 
 print ("Loading Data")
 
-lines = []
+samples = []
 with open('./data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
-    for line in reader:
-        lines.append(line)
+    for sample in reader:
+        samples.append(sample)
 
-images = []
-measurements = []
-correction = 0.2
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-# Loading all center image and some left and right images
-for line in lines:
-    center_image = cv2.imread(get_file_name(line[0]))
-    center_angle = float(line[3])
-    images.extend([center_image])
-    measurements.extend([center_angle])
 
-    if np.random.normal(0.5, 0.5) < 0.3 :
-        left_image = cv2.imread(get_file_name(line[1]))
-        left_angle = center_angle + correction
-        images.extend([left_image])
-        measurements.extend([left_angle])
+def augmented_generator(samples, batch_size):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0,num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+            images =[]
+            measurements = []
 
-    if np.random.normal(0.5, 0.5) < 0.3 :
-        right_image = cv2.imread(get_file_name(line[2]))
-        right_angle = center_angle - correction
-        images.extend([right_image])
-        measurements.extend([right_angle])
+            for batch_sample in batch_samples:
 
-print ("Converting Data")
-X_train = np.array(images)
-y_train = np.array(measurements)
+                # Load Center image and label
+                center_image = cv2.imread(get_file_name(batch_sample[0]))
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                measurements.append(center_angle)
 
-print ("X_train shape: ",X_train.shape)
-print ("y_train shape: ",y_train.shape)
+                # Only flip image when it's turning
+                if abs(center_angle) > 0.3:
+                    center_image,center_angle = mirror(center_image,center_angle)
+                    images.append(center_image)
+                    measurements.append(center_angle)
 
-# Clear memory
-lines = None
-images = None
-measurements = None
+                # Load left camera with correction
+                if np.random.normal(0.5, 0.5) < left_cam_limit :
+                    left_image = cv2.imread(get_file_name(batch_sample[1]))
+                    left_angle = center_angle + steering_correction
+                    images.append(left_image)
+                    measurements.append(left_angle)
 
-###================== Applying Augmentations ==================###
+                # Load right camera with correction
+                if np.random.normal(0.5, 0.5) < right_cam_limit :
+                    right_image = cv2.imread(get_file_name(batch_sample[2]))
+                    right_angle = center_angle - steering_correction
+                    images.append(right_image)
+                    measurements.append(right_angle)
 
-print ("Applying Augmentations")
+                # Load augmented camera with augmented label
+                if np.random.normal(0.5, 0.5) < aug_limit :
+                    center_image,center_angle = augment_data(center_image,center_angle)
+                    images.append(center_image)
+                    measurements.append(center_angle)
 
-aug_img = []
-aug_steering = []
-for i in range(len(y_train)):
-    if np.random.normal(0.5, 0.5) < 0.5 :
-        img,steering = augment_data(X_train[i],y_train[i])
-        aug_img.append(img)
-        aug_steering.append(steering)
+                # Yield once batch size has been fulfilled 
+                if len(measurements) >=  batch_size:
+                    X_train = np.array(images)
+                    y_train = np.array(measurements)
 
-aug_img = np.asarray(aug_img)
-aug_steering = np.asarray(aug_steering)
+                    yield shuffle(X_train, y_train)
+            
 
-print ("Combining Data")
-X_train = np.concatenate([aug_img,X_train])
-y_train = np.concatenate([aug_steering,y_train])
 
-print ("X_train shape: ",X_train.shape)
-print ("y_train shape: ",y_train.shape)
 
-print ("Shuffling Data")
-shuffle(X_train, y_train)
+def generator(samples, batch_size):
+    num_samples = len(samples)
 
-###======================= Model =============================###
+    while 1:
+        shuffle(samples)
+        for offset in range(0,num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images =[]
+            measurements = []
+            for batch_sample in batch_samples:
+                center_image = cv2.imread(get_file_name(batch_sample[0]))
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                measurements.append(center_angle)
+
+            X_train = np.array(images)
+            y_train = np.array(measurements)
+
+            yield shuffle(X_train, y_train)
+
+
+train_generator = augmented_generator(train_samples, batch_size=batch_size)
+validation_generator = generator(validation_samples, batch_size=batch_size)
+
+
+###============================================================###
+###========================== Model ===========================###
+###============================================================###
 
 
 print ("Building Model")
 # Model Architecture
-
-dropout = 0.2
-input_shape =(160,320,3)
 
 model = Sequential()
 # Normalization
@@ -106,9 +158,11 @@ model.add(Dropout(dropout))
 model.add(Dense(10, activation='relu'))
 model.add(Dense(1, activation='linear'))
 
+
 # Mean squared error
 model.compile(loss='mse',optimizer='adam')
-model.fit(X_train,y_train,validation_split=0.2,shuffle=True, epochs=3)
+model.fit_generator(train_generator, samples_per_epoch= len(train_samples),validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=1)
+
 
 # Save the model
 model.save('model.h5')
